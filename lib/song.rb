fb63f723e6a6a6d5f1f5a6d91df38c88dc9b5f24
Marques Avery <averymarques4@gmail.com>
2020-08-19 23:37:57 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -29,7 +29,7 @@ class Song
   end
 
   def self.find_by_name(name)
-    if @@all.any?{|value| value == name}
+    if @@all.any?{|value| value.name == name}
       return value
     else
       return false

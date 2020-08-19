41fb190ebd3c714fbe8073b1c70eedc61ed8a553
Marques Avery <averymarques4@gmail.com>
2020-08-19 22:26:12 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,10 +1,41 @@
+require 'pry'
+
 class Song
   attr_accessor :name, :artist_name
   @@all = []
 
+  def self.create
+    song = self.new
+    @@all << song
+    song
+  end
+
   def self.all
     @@all
   end
+
+  def self.new_by_name(name)
+    song = self.new
+    # binding.pry
+    song.name = name
+    song
+  end
+
+  def self.create_by_name(name)
+    song = Song.new
+    song.name = name
+    @@all << song
+    song
+  end
+
+  def self.find_by_name(name)
+    @@all.each do |value|
+      if value.name = name
+        value
+      end
+    end
+  end
+
 
   def save
     self.class.all << self

fbeaf20bda821d7b9c5c730e7aa9ba0d14ec1562
Marques Avery <averymarques4@gmail.com>
2020-08-20 00:22:49 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -35,8 +35,8 @@ class Song
   end
 
   def self.find_or_create_by_name(name)
-    self.find_by_name(name)
-    self.create_by_name(name)
+    if not self.find_by_name(name)
+      self.create_by_name(name)
 
   end
 

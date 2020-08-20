66f49b0a6889176f7333a02b616312872f729676
Marques Avery <averymarques4@gmail.com>
2020-08-20 00:42:47 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -51,7 +51,7 @@ class Song
     seperate = filename.split("-")
     seperate_song = seperate[1].split(".")
     @artist_name = seperate[1].strip
-    .name = seperate_song[0].strip
+    @name = seperate_song[0].strip
 
 
 
